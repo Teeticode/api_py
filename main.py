2ac19a02d31561c8e6ea4e0a68e1f91dd3448b83
Teeticode <teetinormm@gmail.com>
2022-07-02 10:21:50 +0300

    basic structure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tokenize import String
 from typing import Optional
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, HTTPException, Response, status
 from pydantic import BaseModel
 from random import randrange
 
@@ -26,17 +26,26 @@ def root():
 def get_posts():
     return{"posts":my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 1000000000)
     my_posts.append(post_dict)
     return{"data": post}
 #title str, content str, category, category
+@app.get('/posts/latest')
+def get_latest_post():
+    post = my_posts[len(my_posts)-1]
+    return{"data": post}
 
 @app.get("/posts/{id}")
 def get_post(id: int):
     print(id)
     post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail = f"post with id: {id} was not found")
+        
     print(post)
     return{"post_detail": post}
+
